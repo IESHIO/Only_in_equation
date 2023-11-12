382b680c3f8fcf11ddbbb21fb6f2caa793fea077
Denis B <126201808+IESHIO@users.noreply.github.com>
2023-11-13 02:36:14 +0300

    Подготовка к решению матриц через двумерный массив

--- Test.cpp
+++ Test.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    unsigned short int SelectMethod;
-    SelectMethod = 0;
-    cout<<SelectMethod;
-}
+    const unsigned int ROWS=3;
+    const unsigned int COLS=3;
+    int array [ROWS][COLS];
+    
+    for(int i=0;i<ROWS;i++)
+    {
+        for(int j=0;j<COLS;j++)
+        {
+          cin>>array[i][j]; 
+        }
+    }
+    
+    cout << "Введённая матрица:" << endl;
+    
+    for(int i=0;i<ROWS;i++)
+    {
+        for(int j=0;j<COLS;j++)
+        {
+          cout<<array[i][j]<< "\t"; 
+        }
+        cout<<endl;
+    }
+    
+
+return 0;
+}    
